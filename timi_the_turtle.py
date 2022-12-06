c5f3ebfc2fbec2cf35b4092a9631a63454c7dd1a
SeprenyiBalazs <seprenyi.ba@gmail.com>
2022-12-06 21:01:40 +0100

    drawing different shapes, generate a random walk, generate random RGB colours

--- timi_the_turtle.py
+++ timi_the_turtle.py
@@ -4,9 +4,20 @@ import random
 timi = Turtle()
 colours = ["light steel blue", "green yellow", "royal blue",
            "teal", "light slate gray", "slate gray", "cadet blue"]
+direction = [0, 90, 180, 270]
+timi.pensize(15)
 
 
-def draw_shape(num_sides):
+"""def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color"""
+
+
+# Triangle Turtle
+"""def draw_shape(num_sides):
     angle = 360 / num_sides
     for _ in range(num_sides):
         timi.forward(100)
@@ -15,4 +26,12 @@ def draw_shape(num_sides):
 
 for shape_side_n in range(3, 11):
     timi.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    draw_shape(shape_side_n)"""
+
+# Random walk turtle
+
+
+for _ in range(200):
+    timi.color(random.choice(colours))
+    timi.forward(30)
+    timi.setheading(random.choice(direction))
